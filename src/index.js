ace9c39f374dcb63374fb0560b4805aa84e77efb
ann-sed <a.sedeeva@yandex.ru>
2025-09-10 13:19:17 +0300

    Fix validation to src/index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import onChange from 'on-change'
 import render from './view.js'
 
 const schema = yup.object().shape({
-  url: yup.string().url().required().notOneOf(), // Добавить ошибки для сообщений
+  inputValue: yup.string()
+    .url('Ссылка должна быть валидным URL')
+    .required()
+    .notOneOf(this.existingUrls, 'RSS уже существует'),
 })
-
-// schema.valid({url: new FormData(e.target)})
 
 const app = () => {
   const initialState = {
@@ -23,27 +24,32 @@ const app = () => {
       posts: [],
     },
   }
-  const watchedState = onChange(initialState, () => render(watchedState)) // ?????
-
+  const watchedState = onChange(initialState, () => render(watchedState))
   const form = document.querySelector('.rss-form')
 
   form.addEventListener('submit', (e) => {
     e.preventDefault()
     const formData = new FormData(e.target)
+    const inputValue = formData.get('url')
 
-    if (schema.isValid({ url: formData })) {
+    const existingUrls = watchedState.ui.posts.map(post => post.url)
+    const validateForm = schema.validate(
+      { inputValue },
+      { context: { existingUrls } },
+    )
+
+    validateForm.then(() => {
       watchedState.formState.isValid = true
       watchedState.formState.errors = {}
-
       watchedState.processState.processStatus = 'sending'
       // и мы отправляем запрос на сервер.
       // Далее проверка валидации самого сайта что это rss url
       // От этого зависит статус 'failed', 'success'
-    }
-    else {
-      watchedState.formState.isValid = false
-      watchedState.formState.errors = { massage: 'Ссылка должна быть валидным URL' }
-    }
+    })
+      .catch((error) => {
+        watchedState.formState.isValid = false
+        watchedState.formState.errors = { message: error.massage }
+      })
   })
 }
 
