130ac7d0ed7ac784cf3d25baeb38798fc2b49cab
ann-sed <a.sedeeva@yandex.ru>
2025-09-17 00:28:32 +0300

    Add i18next initialization to src/main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import app from './index.js'
-app()
+import i18next from 'i18next'
+import resources from './locales/ru.js'
+
+const runApp = async () => {
+  const defaultLang = 'ru'
+  const i18n = i18next.createInstance()
+  await i18n.init({
+    lng: defaultLang,
+    debug: false,
+    resources,
+  })
+  app(i18n)
+}
+
+runApp()
