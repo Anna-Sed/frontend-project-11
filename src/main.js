f0d055acf3796cddc53e26c9a9e2bae79f11b85d
ann-sed <a.sedeeva@yandex.ru>
2025-09-17 17:11:36 +0300

    Fix locales import

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import app from './app.js'
 import i18next from 'i18next'
-import resources from './locales/ru.js'
+import resources from './locales/index'
 
 const runApp = async () => {
   const defaultLang = 'ru'
   const i18n = i18next.createInstance()
   await i18n.init({
     lng: defaultLang,
-    debug: false,
+    debug: true,
     resources,
   })
   app(i18n)
