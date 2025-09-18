1ae2ebc6699dceeffbe4a7ea773ae3ec23977495
ann-sed <a.sedeeva@yandex.ru>
2025-09-18 22:27:14 +0300

    Update the processing of form states

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import * as yup from 'yup'
-import onChange from 'on-change'
-import render from './view.js'
+import createFormWatcher from './view.js'
 
 yup.setLocale({
   mixed: {
@@ -15,14 +14,14 @@ yup.setLocale({
 const createRssSchema = existingUrls => yup.string().url().required().notOneOf(existingUrls)
 
 const app = (i18n) => {
-  const initialState = {
+  const state = {
     formState: {
       isValid: null,
       errors: {}, // { message }
       inputValue: '', // ???
     },
     processState: {
-      processStatus: 'filling', // 'sending', 'failed', 'success'
+      status: 'filling', // 'sending', 'failed', 'success'
       processErrors: {},
     },
     ui: {
@@ -30,8 +29,20 @@ const app = (i18n) => {
       posts: [], // { id, url }
     },
   }
-  const watchedState = onChange(initialState, () => render(watchedState))
+
+  const submitButton = document.querySelector('[aria-label="add"]')
+  const rssInput = document.querySelector('#url-input')
+  const feedbackField = document.querySelector('.feedback')
   const form = document.querySelector('.rss-form')
+
+  const elements = {
+    submitButton,
+    rssInput,
+    feedbackField,
+    form,
+  }
+
+  const watchedState = createFormWatcher(state, i18n, elements)
 
   form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -45,14 +56,15 @@ const app = (i18n) => {
     validateForm.then(() => {
       watchedState.formState.isValid = true
       watchedState.formState.errors = {}
-      watchedState.processState.processStatus = 'sending'
+      watchedState.processState.status = 'sending'
       // и мы отправляем запрос на сервер.
       // Далее проверка валидации самого сайта что это rss url
       // От этого зависит статус 'failed', 'success'
+      watchedState.processState.status = 'success'
     })
       .catch((error) => {
         watchedState.formState.isValid = false
-        watchedState.processState.processStatus = 'failed'
+        // watchedState.processState.status = 'failed'
         const errorMessage = i18n.t(error.message)
         console.log('error message = ', errorMessage)
         watchedState.formState.errors = { message: errorMessage }
--- src/view.js
+++ src/view.js
@@ -1,21 +1,62 @@
-const render = (state) => {
-  const { formState } = state
+import onChange from 'on-change'
 
-  // const submitButton = document.querySelector('[aria-label="add"]')
-  const rssInput = document.querySelector('#url-input')
-  const feedbackField = document.querySelector('.feedback')
+const handleProcess = (processState, errors, elements, i18n) => { // передача ошибки именно из processState
+  const { rssInput, feedbackField, submitBtn } = elements
+  switch (processState) {
+    case 'filling':
+      break
+    case 'failed':
+      submitBtn.disabled = false
+      rssInput.disabled = false
+      feedbackField.classList.add('text-danger')
+      feedbackField.textContent = errors.message
+      break
+    case 'success':
+      submitBtn.disabled = false
+      rssInput.disabled = false
+      feedbackField.textContent = i18n.t('rss_form.success_message')
+      feedbackField.classList.remove('text-danger')
+      rssInput.focus()
+      rssInput.value = ''
+      break
+    case 'sending':
+      submitBtn.disabled = true
+      rssInput.disabled = true
+      break
+    default:
+      throw new Error(`Unexpected process state: ${processState}`)
+  }
+}
 
-  if (formState.isValid) {
+const handleIsValid = (isValid, elements, errors) => { // передача ошибки именно валидации из formState
+  const { rssInput, feedbackField } = elements
+  if (isValid) {
     feedbackField.textContent = ''
     rssInput.classList.remove('is-invalid')
     rssInput.classList.add('is-valid')
   }
   else {
     rssInput.classList.add('is-invalid')
-    console.log('error in view =', formState.errors.message)
-    feedbackField.textContent = formState.errors.message
+    console.log('error in view =', errors.message)
+    feedbackField.classList.add('text-danger')
+    feedbackField.textContent = errors.message
     console.log('text content = ', feedbackField.textContent)
   }
 }
+const createFormWatcher = (state, i18n, elements) => {
+  const { formState, processState } = state
+  return onChange(state, (path, value) => {
+    switch (path) {
+      case 'formState.isValid':
+        handleIsValid(value, elements, formState.errors)
+        break
+      case 'processState.status':
+        handleProcess(value, processState.processErrors, elements, i18n)
+        break
+      default:
+        throw new Error(`Unexpected path: ${path}`)
+    }
+  })
+}
 
-export default render
+export default createFormWatcher
