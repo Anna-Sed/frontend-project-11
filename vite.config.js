384505631457674b9feecb09c0a6ed7a1dd6950e
ann-sed <a.sedeeva@yandex.ru>
2026-05-19 22:44:00 +0300

    Update the vite.config.js (added noDiscovery and include in config)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   optimizeDeps: {
     entries: [],
-    disabled: true,
+    noDiscovery: true, // отключает автоматический поиск зависимостей
+    include: undefined, // не включает никакие зависимости в предсборку
   },
 })
