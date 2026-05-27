6b87223c16f2baa8adf5d7e925c23a9dd9f280c1
ann-sed <a.sedeeva@yandex.ru>
2026-05-28 00:31:28 +0300

    Split the render into modules in src/watchers/feedWatcher.js

--- src/watchers/feedWatcher.js
+++ src/watchers/feedWatcher.js
@@ -1,80 +1,6 @@
 import onChange from 'on-change'
-
-const createConteiner = (root, title) => {
-  const card = document.createElement('div')
-  card.classList.add('card', 'border-0')
-  const childrenHtml = `<div class="card-body">
-      <h2 class="card-title h4">${title}</h2>
-    </div> 
-    <ul class="list-group border-0 rounded-0"></ul>
-  `
-  card.innerHTML = childrenHtml
-  root.replaceChildren(card)
-  return card
-}
-
-const renderFeeds = (feeds, feedsRoot, i18n) => {
-  // console.log('Рендерим фиды:', feeds)
-  const feedsTitle = i18n.t('feeds_title')
-  const conteiner = createConteiner(feedsRoot, feedsTitle)
-  const ul = conteiner.querySelector('ul')
-
-  const liItems = feeds.map((feed) => {
-    const li = document.createElement('li')
-    li.classList.add('list-group-item', 'border-0', 'border-end-0')
-    const liChild = `<h3 class="h6 m-0">${feed.title}</h3>
-    <p class="m-0 small text-black-50">${feed.description}</p>
-    `
-
-    li.innerHTML = liChild
-    return li
-  })
-
-  ul.append(...liItems)
-}
-
-const createPostItem = (post, seenPost, i18n) => {
-  const { id, link, title, description } = post
-  const a = document.createElement('a')
-  const postClass = seenPost.includes(id) ? ['fn-normal', 'link-secondary'] : ['fw-bold']
-  a.classList.add(...postClass)
-  a.setAttribute('data-id', id)
-  a.setAttribute('target', '_blank')
-  a.textContent = title
-  a.href = link
-  a.addEventListener('click', () => seenPost.push(id))
-
-  const button = document.createElement('button')
-  button.type = 'button'
-  button.classList.add('btn', 'btn-outline-primary', 'btn-sm')
-  button.setAttribute('data-id', id)
-  button.setAttribute('data-bs-toggle', 'modal')
-  button.setAttribute('data-bs-target', '#modal')
-  button.textContent = i18n.t('post_button')
-  // Вопрос - зачем вот это??? |
-  button.dataset.bsTitle = title
-  button.dataset.bsDescription = description
-  button.dataset.bsLink = link
-
-  const li = document.createElement('li')
-  li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0')
-  li.append(a, button)
-  return li
-}
-
-const renderPost = (posts, seenPost, postsRoot, i18n) => {
-  // console.log('Рендерим посты:', posts)
-  if (posts.length === 0) {
-    postsRoot.innerHtml = ''
-    return
-  }
-
-  const postsTitle = i18n.t('posts_title')
-  const conteiner = createConteiner(postsRoot, postsTitle)
-  const ul = conteiner.querySelector('ul')
-  const liItems = posts.map(post => createPostItem(post, seenPost, i18n))
-  ul.append(...liItems)
-}
+import renderFeeds from './../render/renderFeeds.js'
+import renderPosts from './../render/renderPosts.js'
 
 const createFeedsWatcher = (feedsData, uiState, elements, i18n) => {
   const { postsRoot, feedsRoot } = elements
@@ -82,7 +8,7 @@ const createFeedsWatcher = (feedsData, uiState, elements, i18n) => {
     // console.log('путь  = ', path, `и значение = ${value}`)
     switch (path) {
       case 'posts':
-        renderPost(value, uiState.seenPost, postsRoot, i18n)
+        renderPosts(value, uiState.seenPost, postsRoot, i18n)
         break
       case 'feeds':
         renderFeeds(value, feedsRoot, i18n)
