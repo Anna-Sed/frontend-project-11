1033d8632ac3fefe25fbccce04e839fdc3a2ccc1
ann-sed <a.sedeeva@yandex.ru>
2026-05-27 14:27:42 +0300

    Add to src/app.js the eventListener for render modal

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const addProxy = (url) => {
 const downloadRssFeed = url => axios
   .get(addProxy(url))
   .then((response) => {
-    console.log('Полный ответ:', JSON.stringify(response.data, null, 2))
+    // console.log('Полный ответ:', JSON.stringify(response.data, null, 2))
     return parseRss(response.data.contents)
   })
   .catch ((error) => {
@@ -56,6 +56,7 @@ const app = (i18n) => {
     },
     uiState: {
       seenPost: [],
+      modalId: null,
     },
   }
 
@@ -66,6 +67,7 @@ const app = (i18n) => {
     form: document.querySelector('.rss-form'),
     postsRoot: document.querySelector('.posts'),
     feedsRoot: document.querySelector('.feeds'),
+    modalContainer: document.querySelector('.modal-content'),
   }
 
   const watchedFormState = createFormWatcher(state, i18n, elements)
@@ -76,23 +78,23 @@ const app = (i18n) => {
       const allFeedLinks = state.feedsData.feeds.map(feed => feed.resource)
       if (allFeedLinks.length === 0) interval()
       else {
-        console.log('Все ссылки фидов в интервале', allFeedLinks)
+        // console.log('Все ссылки фидов в интервале', allFeedLinks)
         const allPromisePosts = allFeedLinks
           .map(link => downloadRssFeed(link)
             .then(({ posts }) => posts))
-        console.log('allPromisePosts в интервале: ', allPromisePosts)
+        // console.log('allPromisePosts в интервале: ', allPromisePosts)
         Promise.all(allPromisePosts)
           .then((allPosts) => {
-            console.log('Все посты в интервале после загрузки аксиос: ', allPosts)
+            // console.log('Все посты в интервале после загрузки аксиос: ', allPosts)
             const newPosts = allPosts
               .flat()
               .filter((post) => {
                 const { posts } = state.feedsData
                 return !posts.find(el => el.link === post.link)
               })
-            console.log('Новые посты в интервале: ', newPosts)
+            // console.log('Новые посты в интервале: ', newPosts)
             watchedFeedsState.posts = [...newPosts, ...state.feedsData.posts]
-            console.log('стейт с постами в интервале после добавления новых постов: ', state.feedsData.posts)
+            // console.log('стейт с постами в интервале после добавления новых постов: ', state.feedsData.posts)
           })
           .catch((error) => {
             console.error('Ошибка при загрузке RSS-фидов:', error)
@@ -104,10 +106,10 @@ const app = (i18n) => {
 
   interval()
 
-  if (!elements.feedsRoot || !elements.postsRoot) {
-    console.error('Не найдены элементы для рендеринга')
-    return
-  }
+  // if (!elements.feedsRoot || !elements.postsRoot) {
+  //   console.error('Не найдены элементы для рендеринга')
+  //   return
+  // }
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -131,26 +133,26 @@ const app = (i18n) => {
         feed.resource = inputValue
         watchedFeedsState.feeds = [feed, ...watchedFeedsState.feeds]
         watchedFeedsState.posts = [...posts, ...watchedFeedsState.posts]
-        console.log('стейт после получения данных с сервера = ', watchedFeedsState)
-        console.log('url = ', inputValue)
+        // console.log('стейт после получения данных с сервера = ', watchedFeedsState)
+        // console.log('url = ', inputValue)
         watchedFeedsState.urls = [inputValue, ...watchedFeedsState.urls]
       })
       .catch((error) => {
         if (error instanceof yup.ValidationError) {
           watchedFormState.formState.isValid = false
           const errorMessage = i18n.t(error.message)
-          console.log('error message = ', errorMessage)
+          // console.log('error message = ', errorMessage)
           watchedFormState.formState.errors = { message: errorMessage }
-          console.log('state form = ', watchedFormState)
+          // console.log('state form = ', watchedFormState)
         }
         else {
           watchedFormState.processState.status = 'failed'
-          console.log('ошибка сети до перевода - ', error.message)
+          // console.log('ошибка сети до перевода - ', error.message)
           const message = i18n.t(error.message)
-          console.log('ошибка сети после перевода - ', message)
+          // console.log('ошибка сети после перевода - ', message)
           watchedFormState.processState.processErrors = { message }
-          console.log('processState.processErrors = ', watchedFormState.processState.processErrors)
-          console.log('state feed = ', watchedFeedsState)
+          // console.log('processState.processErrors = ', watchedFormState.processState.processErrors)
+          // console.log('state feed = ', watchedFeedsState)
 
           if (message === 'Ресурс не содержит валидный RSS') {
             watchedFormState.formState.isValid = false
@@ -158,6 +160,18 @@ const app = (i18n) => {
           }
         }
       })
+  })
+
+  elements.postsRoot.addEventListener('click', (e) => {
+    if (!event.target.dataset.id) return
+
+    const postId = e.target.dataset.id
+    console.log('Нажатый пост: ', postId)
+    watchedFormState.uiState.modalId = postId
+    console.log('состояние модального айди: ', state.uiState.modalId)
+    if (!state.uiState.seenPost.includes(postId)) {
+      state.uiState.seenPost.push(postId)
+    }
   })
 }
 
