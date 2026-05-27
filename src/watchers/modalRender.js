515b1df6e49c8bdf213460d5ee19445abfdc571c
ann-sed <a.sedeeva@yandex.ru>
2026-05-27 14:25:57 +0300

    Add the src/watchers/modalRender.js

---
+++ src/watchers/modalRender.js
@@ -0,0 +1,29 @@
+export default (state, i18n, elements) => {
+  console.log('Рендер модального окна')
+  const { modalContainer } = elements
+
+  const modalReadBtn = modalContainer.querySelector('.read_btn')
+  const modalCloseBtn = modalContainer.querySelector('.close_btn')
+  const modalTitle = modalContainer.querySelector('.modal-title')
+  const modalBody = modalContainer.querySelector('.modal-body')
+
+  modalCloseBtn.textContent = i18n.t('modal.modal_btn_close')
+  modalReadBtn.textContent = i18n.t('modal.modal_btn_reed_post')
+
+  const { modalId } = state.uiState
+  const currentPost = state.feedsData.posts.find(post => post.id === modalId)
+  const { title, description, link } = currentPost
+  modalTitle.textContent = title
+  modalBody.textContent = description
+
+  const handleClick = () => {
+    window.open(link, '_blank', 'noopener, noreferrer')
+  }
+
+  modalReadBtn.addEventListener('click', handleClick)
+
+  // Удаление обработчика. Чтобы избежать утечек памяти, удаляйте обработчик при закрытии модального окна
+  // document.addEventListener('hidden.bs.modal', () => {
+  //   modalReadBtn.removeEventListener('click', handleClick)
+  // })
+}
