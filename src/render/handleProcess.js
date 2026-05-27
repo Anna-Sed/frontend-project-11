ad982b43849b7bc23b5231d249d5b3d6b3e114fd
ann-sed <a.sedeeva@yandex.ru>
2026-05-28 00:56:46 +0300

    Create module-handleProcess.js

---
+++ src/render/handleProcess.js
@@ -0,0 +1,30 @@
+// Рендер формы в зависимости от статуса процесса - успех, заполнение, отправка
+// передача ошибки именно из processState
+
+export default (processStatus, errors, elements, i18n) => {
+  const { rssInput, feedbackField, submitBtn } = elements
+  switch (processStatus) {
+    case 'filling':
+      break
+    case 'failed':
+      submitBtn.disabled = false
+      rssInput.disabled = false
+      feedbackField.classList.add('text-danger')
+      feedbackField.textContent = errors.message
+      break
+    case 'success':
+      submitBtn.disabled = false
+      rssInput.disabled = false
+      feedbackField.textContent = i18n.t('rss_form.success_message')
+      feedbackField.classList.remove('text-danger')
+      rssInput.focus()
+      rssInput.value = ''
+      break
+    case 'sending':
+      submitBtn.disabled = true
+      rssInput.disabled = true
+      break
+    default:
+      throw new Error(`Unexpected process state: ${processStatus}`)
+  }
+}
